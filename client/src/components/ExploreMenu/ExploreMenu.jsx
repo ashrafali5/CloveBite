11f9cd7ee300b8987befc5c130b79b1afce89166
Ashraf Ali <ashrafmohd83517@gmail.com>
2025-06-14 06:28:29 +0530

    commit

--- client/src/components/ExploreMenu/ExploreMenu.jsx
+++ client/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,9 +1,38 @@
-import './'
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
-    <div>
-      <h1>ExploreMenu</h1>
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Discover a world of flavors at your fingertips. Our menu is carefully
+        curated to satisfy every craving — from fresh salads to indulgent
+        desserts.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "" : item.menu_name
+                )
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt=""
+              />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
     </div>
   );
 };
