f102a90b668737ef4a1c96959a471db1cb395503
Ashraf Ali <ashrafmohd83517@gmail.com>
2025-06-16 07:55:05 +0530

    commit

---
+++ client/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,16 @@
+import "./FoodItem.css";
+
+const FoodItem = ({ id, name, price, description, image }) => {
+  return (
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img className="food-item-img" src={image} alt={name} />
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating"></div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodItem;
